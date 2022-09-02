ce23c89bfab38dad1d83fef0f0ec43aac4095b96
Christopher Kennedy <csk86.05.14@gmail.com>
2022-09-02 09:42:09 -0500

    formatting ii

--- src/main.c
+++ src/main.c
@@ -4,10 +4,8 @@
 
 void add_todo(int todo_length, char** todo, char* todo_file);
 
-int main(int argc, char** argv)
-{
-    if (argc == 1)
-    {
+int main(int argc, char** argv) {
+    if (argc == 1) {
         printf("Need at least one argument!\n");
         return -1;
     }
@@ -17,46 +15,37 @@ int main(int argc, char** argv)
     return 0;
 }
 
-void add_todo(int todo_length, char** todo, char* todo_file)
-{
+void add_todo(int todo_length, char** todo, char* todo_file) {
     FILE* fp;
     fp = fopen(todo_file, "a+");
     char long_todo[4096] = { '\0' };
 
-    if (long_todo == NULL)
-    {
+    if (long_todo == NULL) {
         printf("Cannot get todo\n");
         return;
     }
 
-    if (todo_length > 2)
-    {
-        for (int i = 2; i < todo_length; i++)
-        {
-            if (long_todo == 0)
-            {
+    if (todo_length > 2) {
+        for (int i = 2; i < todo_length; i++) {
+            if (long_todo == 0) {
                 return;
             }
-            else
-            {
+            else {
                 strncat(long_todo, todo[i], sizeof(long_todo) - 1);
                 strncat(long_todo, " ", sizeof(long_todo) - 1);
             }
         }
     }
-    else
-    {
-        if (long_todo == 0)
-        {
+    else {
+        if (long_todo == 0) {
             return;
         }
-        else
-        {
+        else {
             strncat(long_todo, todo[2], sizeof(long_todo) - 1);
         }
     }
     strncat(long_todo, "\n", sizeof(long_todo) - 1);
-  
+
     long_todo[4095] = '\0';
 
     fprintf(fp, long_todo);
